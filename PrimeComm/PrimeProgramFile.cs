2663a8e7e3dbca9129065cfd7d67ac4d76b11cc0
Erwin Ried <erwinried@gmail.com>
2013-11-20 14:59:15 -0300

    Support for all other kind of files

--- PrimeComm/PrimeProgramFile.cs
+++ PrimeComm/PrimeProgramFile.cs
@@ -14,7 +14,8 @@ namespace PrimeComm
         /// Parses the data inside a file to be used later
         /// </summary>
         /// <param name="path">Input file</param>
-        public PrimeProgramFile(string path)
+        /// <param name="ignoreInternal">Ignore the internal name for the file</param>
+        public PrimeProgramFile(string path, bool ignoreInternal = true)
         {
             IsValid = false;
             Name = Path.GetFileNameWithoutExtension(path);
@@ -27,20 +28,49 @@ namespace PrimeComm
                     Data = Encoding.Convert(Encoding.Default, Encoding.Unicode, b);
                     IsValid = true;
                     break;
-                    
+                  
+                case null:
+                    break; 
+
                 default:
                     if (b.Length >= 20)
                     {
-                        if (b[0] == 0x0c && b[8] == 0x00) // Unnamed and supported
+                        for (var i = 1; i <= 7; i++)
+                            if (b[i] != 0x00)
+                                goto case null;
+
+                        switch (b[8])
                         {
-                            var size = b[16] + b[17]*0xff + b[18]*0xff*0xff;
-                            Data = new byte[size];
+                            case 0x00:
+                                var size = b[16] + b[17]*0xff + b[18]*0xff*0xff;
+                                Data = new byte[size];
 
-                            const int offset = 20;
-                            for (int i = offset; i < offset + size && i < b.Length; i++)
-                                Data[i - offset] = b[i];
+                                const int offset = 20;
+                                for (int i = offset; i < offset + size && i < b.Length; i++)
+                                    Data[i - offset] = b[i];
 
-                            IsValid = true;
+                                IsValid = true;
+
+                                break;
+
+                            case 0x01:
+                                if (b[16] == 0x31)
+                                {
+                                    var pivot = -1;
+                                    for(var i=18;i<b.Length;i++)
+                                        if (b[i - 1] == b[i] && b[i] == 0x00)
+                                        {
+                                            if (!ignoreInternal)
+                                                Name = Encoding.Unicode.GetString(b.SubArray(18, i-18));
+
+                                            i += 8;
+                                            Data = b.SubArray(i, b.Length - i);
+                                            IsValid = true;
+                                            break;
+                                        }
+                                }
+
+                                break;
                         }
                     }
                     break;
