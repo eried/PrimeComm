f0b6c8fa3b930f49d43705aa0d073c8d544a3969
Erwin Ried <erwinried@gmail.com>
2013-11-20 11:07:54 -0300

    Load from plain .txt files

--- PrimeComm/PrimeProgramFile.cs
+++ PrimeComm/PrimeProgramFile.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
 
 namespace PrimeComm
 {
@@ -7,27 +10,40 @@ namespace PrimeComm
     /// </summary>
     class PrimeProgramFile
     {
+        /// <summary>
+        /// Parses the data inside a file to be used later
+        /// </summary>
+        /// <param name="path">Input file</param>
         public PrimeProgramFile(string path)
         {
             IsValid = false;
             Name = Path.GetFileNameWithoutExtension(path);
             Data = new byte[0];
-
             var b = File.ReadAllBytes(path);
 
-            if (b.Length >= 20)
+            switch (Path.GetExtension(path))
             {
-                if (b[0] == 0x0c && b[8] == 0x00) // Unnamed and supported
-                {
-                    var size = b[16] + b[17]*0xff + b[18]*0xff*0xff;
-                    Data = new byte[size];
-
-                    const int offset = 20;
-                    for (int i = offset; i < offset + size && i < b.Length; i++)
-                        Data[i - offset] = b[i];
-
+                case ".txt":
+                    Data = Encoding.Convert(Encoding.Default, Encoding.Unicode, b);
                     IsValid = true;
-                }
+                    break;
+                    
+                default:
+                    if (b.Length >= 20)
+                    {
+                        if (b[0] == 0x0c && b[8] == 0x00) // Unnamed and supported
+                        {
+                            var size = b[16] + b[17]*0xff + b[18]*0xff*0xff;
+                            Data = new byte[size];
+
+                            const int offset = 20;
+                            for (int i = offset; i < offset + size && i < b.Length; i++)
+                                Data[i - offset] = b[i];
+
+                            IsValid = true;
+                        }
+                    }
+                    break;
             }
         }
 
