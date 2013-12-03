5063f65fa8b2a40fe765fb5fdf73a04a1ccffedb
Erwin Ried <erwinried@gmail.com>
2013-12-03 01:04:38 -0300

    New command line options

--- PrimeCmd/Options.cs
+++ PrimeCmd/Options.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using CommandLine;
@@ -12,7 +14,7 @@ namespace PrimeCmd
         [Option('i', "input", MetaValue="FILE", HelpText = "File to be sent to the device or folder (output).")]
         public string SendFile { get; set; }
 
-        [Option('r', "receive", MetaValue = "FILE", HelpText = "Receive a file from the device.")]
+        [Option('s', "save", MetaValue = "FILE", HelpText = "Receive a file from the device.")]
         public string ReceiveFile { get; set; }
 
         [Option('o', "output", MetaValue = "FOLDER", HelpText = "Destination folder (if empty, send to device).")]
@@ -21,8 +23,14 @@ namespace PrimeCmd
         [Option('t', MetaValue="SECONDS",HelpText = "Timeout in seconds", DefaultValue=5)]
         public int Timeout { get; set; }
 
-        [Option("IgnoreInternalName", HelpText = "Ignore internal name", DefaultValue = false)]
+        [Option("ignore-name", HelpText = "Ignore internal name", DefaultValue = false)]
         public bool IgnoreInternalName { get; set; }
+
+        [Option('c',"server", MutuallyExclusiveSet="server", HelpText = "Starts the command server mode")]
+        public bool RemoteMode { get; set; }
+
+        [OptionList("server-mode", MutuallyExclusiveSet = "server", Separator = ',', HelpText = "Configure server mode, use comma as separator")]
+        public IList<String> RemoteModeConfiguration { get; set; }
 
         /*
         [Option("i", HelpText = "If file has errors don't stop processing.")]
@@ -52,6 +60,27 @@ namespace PrimeCmd
         {
             var help = HelpText.AutoBuild(this);
 
+            var p = Path.GetFileNameWithoutExtension(Process.GetCurrentProcess().MainModule.FileName).ToUpper();
+            var s = new String(' ', p.Length + 21);
+            help.AddPreOptionsLine(" ");
+            help.AddPreOptionsLine("This application allows you to interact with your HP Prime Calculator via");
+            help.AddPreOptionsLine("command line. Call the application using a filename (.txt or .hpprgm) as a");
+            help.AddPreOptionsLine("shortcut for -input argument.");
+
+            help.AddPostOptionsLine("Available modes for Command server mode:");
+            help.AddPostOptionsLine(s + String.Join(", ",Enum.GetNames(typeof (RemoteModes))));
+
+            help.AddPostOptionsLine(" ");
+            help.AddPostOptionsLine("Examples:");
+            help.AddPostOptionsLine(p + " FILE                Send FILE to the device");
+            help.AddPostOptionsLine(p + " -i FILE             Send FILE to the device");
+            help.AddPostOptionsLine(p + " -i FILE -o FOLDER   Convert FILE to .hpprgm and save it to FOLDER");
+            help.AddPostOptionsLine(p + " -i FILE -r FILE2    Open FILE and save it to FILE2");
+            help.AddPostOptionsLine(p + " -r FILE             Receive FILE from the device");
+            help.AddPostOptionsLine(p + " -r FILE -t 10       Receive FILE from the device, waiting 10 secs");
+            help.AddPostOptionsLine(s + "as max for the device to be connected");
+            help.AddPostOptionsLine(p + " -o FOLDER           Receive an .hpprgm and save it to FOLDER");
+
             /*if (LastParserState.Errors.Count > 0)
             {
                 var errors = help.RenderParsingErrorsText(this, 2); // indent with two spaces
@@ -61,8 +90,15 @@ namespace PrimeCmd
                     help.AddPreOptionsLine(errors);
                 }
             }*/
-
             return help;
         }
     }
+
+
+    public enum RemoteModes
+    {
+        quiet,
+        skip_remote_echo,
+        skip_local_echo,
+    };
 }
