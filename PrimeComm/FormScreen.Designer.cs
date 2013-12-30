8d905708d8738a788d428a4fbffeeda570432f3b
Erwin Ried <erwinried@gmail.com>
2013-12-30 01:23:32 -0300

    Screen capture form

---
+++ PrimeComm/FormScreen.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace PrimeComm
+{
+    partial class FormScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormScreen));
+            this.pictureBoxScreen = new System.Windows.Forms.PictureBox();
+            this.buttonCaptureFrame = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.buttonCaptureStream = new System.Windows.Forms.Button();
+            this.buttonFullscreen = new System.Windows.Forms.Button();
+            this.contextMenuStripMain = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.captureFrameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.captureStreamToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.fullscreenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.panelOptions = new System.Windows.Forms.Panel();
+            this.statusStripMain = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxScreen)).BeginInit();
+            this.contextMenuStripMain.SuspendLayout();
+            this.panelOptions.SuspendLayout();
+            this.statusStripMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBoxScreen
+            // 
+            this.pictureBoxScreen.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxScreen.BackColor = System.Drawing.Color.Black;
+            this.pictureBoxScreen.ContextMenuStrip = this.contextMenuStripMain;
+            this.pictureBoxScreen.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxScreen.Name = "pictureBoxScreen";
+            this.pictureBoxScreen.Size = new System.Drawing.Size(457, 376);
+            this.pictureBoxScreen.TabIndex = 0;
+            this.pictureBoxScreen.TabStop = false;
+            // 
+            // buttonCaptureFrame
+            // 
+            this.buttonCaptureFrame.Location = new System.Drawing.Point(93, 3);
+            this.buttonCaptureFrame.Name = "buttonCaptureFrame";
+            this.buttonCaptureFrame.Size = new System.Drawing.Size(75, 23);
+            this.buttonCaptureFrame.TabIndex = 1;
+            this.buttonCaptureFrame.Text = "&Frame";
+            this.buttonCaptureFrame.UseVisualStyleBackColor = true;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClose.Location = new System.Drawing.Point(394, 3);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 1;
+            this.buttonClose.Text = "&Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            // 
+            // buttonCaptureStream
+            // 
+            this.buttonCaptureStream.Location = new System.Drawing.Point(174, 3);
+            this.buttonCaptureStream.Name = "buttonCaptureStream";
+            this.buttonCaptureStream.Size = new System.Drawing.Size(75, 23);
+            this.buttonCaptureStream.TabIndex = 1;
+            this.buttonCaptureStream.Text = "&Stream";
+            this.buttonCaptureStream.UseVisualStyleBackColor = true;
+            // 
+            // buttonFullscreen
+            // 
+            this.buttonFullscreen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonFullscreen.Location = new System.Drawing.Point(313, 3);
+            this.buttonFullscreen.Name = "buttonFullscreen";
+            this.buttonFullscreen.Size = new System.Drawing.Size(75, 23);
+            this.buttonFullscreen.TabIndex = 1;
+            this.buttonFullscreen.Text = "F&ullscreen";
+            this.buttonFullscreen.UseVisualStyleBackColor = true;
+            this.buttonFullscreen.Click += new System.EventHandler(this.buttonFullscreen_Click);
+            // 
+            // contextMenuStripMain
+            // 
+            this.contextMenuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.captureFrameToolStripMenuItem,
+            this.captureStreamToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.fullscreenToolStripMenuItem});
+            this.contextMenuStripMain.Name = "contextMenuStripMain";
+            this.contextMenuStripMain.Size = new System.Drawing.Size(175, 76);
+            this.contextMenuStripMain.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStripMain_Opening);
+            // 
+            // captureFrameToolStripMenuItem
+            // 
+            this.captureFrameToolStripMenuItem.Name = "captureFrameToolStripMenuItem";
+            this.captureFrameToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F8;
+            this.captureFrameToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.captureFrameToolStripMenuItem.Text = "&Capture frame";
+            // 
+            // captureStreamToolStripMenuItem
+            // 
+            this.captureStreamToolStripMenuItem.Name = "captureStreamToolStripMenuItem";
+            this.captureStreamToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F9;
+            this.captureStreamToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.captureStreamToolStripMenuItem.Text = "&Capture stream";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(171, 6);
+            // 
+            // fullscreenToolStripMenuItem
+            // 
+            this.fullscreenToolStripMenuItem.Name = "fullscreenToolStripMenuItem";
+            this.fullscreenToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F11;
+            this.fullscreenToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.fullscreenToolStripMenuItem.Text = "Fullscreen";
+            this.fullscreenToolStripMenuItem.Click += new System.EventHandler(this.fullscreenToolStripMenuItem_Click);
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(12, 3);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrowse.TabIndex = 1;
+            this.buttonBrowse.Text = "Save &to...";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            // 
+            // panelOptions
+            // 
+            this.panelOptions.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelOptions.Controls.Add(this.buttonBrowse);
+            this.panelOptions.Controls.Add(this.buttonCaptureFrame);
+            this.panelOptions.Controls.Add(this.buttonCaptureStream);
+            this.panelOptions.Controls.Add(this.buttonClose);
+            this.panelOptions.Controls.Add(this.buttonFullscreen);
+            this.panelOptions.Location = new System.Drawing.Point(0, 391);
+            this.panelOptions.Name = "panelOptions";
+            this.panelOptions.Size = new System.Drawing.Size(481, 30);
+            this.panelOptions.TabIndex = 3;
+            // 
+            // statusStripMain
+            // 
+            this.statusStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelStatus});
+            this.statusStripMain.Location = new System.Drawing.Point(0, 426);
+            this.statusStripMain.Name = "statusStripMain";
+            this.statusStripMain.Size = new System.Drawing.Size(481, 22);
+            this.statusStripMain.TabIndex = 4;
+            this.statusStripMain.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelStatus
+            // 
+            this.toolStripStatusLabelStatus.Name = "toolStripStatusLabelStatus";
+            this.toolStripStatusLabelStatus.Size = new System.Drawing.Size(39, 17);
+            this.toolStripStatusLabelStatus.Text = "Ready";
+            // 
+            // FormScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 448);
+            this.Controls.Add(this.pictureBoxScreen);
+            this.Controls.Add(this.statusStripMain);
+            this.Controls.Add(this.panelOptions);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PrimeScreen";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxScreen)).EndInit();
+            this.contextMenuStripMain.ResumeLayout(false);
+            this.panelOptions.ResumeLayout(false);
+            this.statusStripMain.ResumeLayout(false);
+            this.statusStripMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxScreen;
+        private System.Windows.Forms.Button buttonCaptureFrame;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Button buttonCaptureStream;
+        private System.Windows.Forms.Button buttonFullscreen;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripMain;
+        private System.Windows.Forms.ToolStripMenuItem captureFrameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem captureStreamToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem fullscreenToolStripMenuItem;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.Panel panelOptions;
+        private System.Windows.Forms.StatusStrip statusStripMain;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelStatus;
+    }
+}
---
+++ PrimeComm/FormScreen.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using PrimeComm.Properties;
+
+namespace PrimeComm
+{
+    public partial class FormScreen : Form
+    {
+        public FormScreen()
+        {
+            InitializeComponent();
+            IsFullscreen = false;
+            LastWindowState = WindowState;
+        }
+
+        private void buttonFullscreen_Click(object sender, EventArgs e)
+        {
+            if (!Settings.Default.SkipFullscreenWarning)
+            {
+                MessageBox.Show("You are enabling Fullscreen mode now. Right click and uncheck fullscreen to exit.",
+                    "Fullscreen mode", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Settings.Default.SkipFullscreenWarning = true;
+                Settings.Default.Save();
+            }
+
+            SetFullscreen(true);
+        }
+
+        private void SetFullscreen(bool p)
+        {
+            IsFullscreen = p;
+            pictureBoxScreen.Dock = p ? DockStyle.Fill: DockStyle.None;
+            panelOptions.Visible = !p;
+            statusStripMain.Visible = !p;
+            FormBorderStyle = !p ? FormBorderStyle.Sizable: FormBorderStyle.None;
+            TopMost = p;
+
+            if (p)
+            {
+                LastWindowState = WindowState;
+                WindowState = FormWindowState.Maximized;
+            }
+            else
+                WindowState = LastWindowState;
+        }
+
+        public FormWindowState LastWindowState { get; set; }
+
+        public bool IsFullscreen { get; set; }
+
+        private void contextMenuStripMain_Opening(object sender, CancelEventArgs e)
+        {
+            fullscreenToolStripMenuItem.Checked = IsFullscreen;
+        }
+
+        private void fullscreenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SetFullscreen(!IsFullscreen);
+        }
+    }
+}
