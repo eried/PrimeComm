03239a56f3f1c7d2e83a80f4a2c38b66181a99cd
Erwin Ried <erwinried@gmail.com>
2014-01-20 19:10:10 -0300

    Only 1 instance allowed

--- PrimeComm/Program.cs
+++ PrimeComm/Program.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace PrimeComm
 {
     static class Program
     {
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, uint nCmdShow);
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -13,7 +22,31 @@ namespace PrimeComm
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain());
+            var instance = false;
+            var mutex = new Mutex(true, Application.ProductName, out instance);
+
+            if (!instance)
+            {
+                // Search for instances of this application
+                var c = Process.GetCurrentProcess();
+                var firstSeen = false;
+
+                foreach (var p in Process.GetProcessesByName(c.ProcessName))
+                    if (p.Id != c.Id)
+                        if (!firstSeen)
+                        {
+                            firstSeen = true;
+                            ShowWindow(p.MainWindowHandle, 5);
+                            SetForegroundWindow(p.MainWindowHandle);
+                        }
+                        else
+                            p.Kill();
+            }
+            else
+            {
+                Application.Run(new FormMain());
+                GC.KeepAlive(mutex);
+            }
         }
     }
 }
