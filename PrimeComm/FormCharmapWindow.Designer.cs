8a76bba24b28659082c7582565386f1af6df3b7c
Erwin Ried <erwinried@gmail.com>
2014-01-23 13:56:15 -0300

    Early version of the Charmap window

---
+++ PrimeComm/FormCharmapWindow.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace PrimeComm
+{
+    partial class FormCharmapWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCharmapWindow));
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.charmap = new PrimeComm.CharmapGrid();
+            this.SuspendLayout();
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.vScrollBar1.Location = new System.Drawing.Point(652, 12);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(17, 154);
+            this.vScrollBar1.TabIndex = 1;
+            // 
+            // charmap
+            // 
+            this.charmap.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.charmap.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.charmap.Location = new System.Drawing.Point(12, 12);
+            this.charmap.Name = "charmap";
+            this.charmap.Size = new System.Drawing.Size(640, 154);
+            this.charmap.TabIndex = 0;
+            // 
+            // FormCharmapWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(678, 178);
+            this.CloseButton = false;
+            this.CloseButtonVisible = false;
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.charmap);
+            this.DockAreas = ((WeifenLuo.WinFormsUI.Docking.DockAreas)(((((WeifenLuo.WinFormsUI.Docking.DockAreas.Float | WeifenLuo.WinFormsUI.Docking.DockAreas.DockLeft) 
+            | WeifenLuo.WinFormsUI.Docking.DockAreas.DockRight) 
+            | WeifenLuo.WinFormsUI.Docking.DockAreas.DockTop) 
+            | WeifenLuo.WinFormsUI.Docking.DockAreas.DockBottom)));
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormCharmapWindow";
+            this.Text = "Characters";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CharmapGrid charmap;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+    }
+}
---
+++ PrimeComm/FormCharmapWindow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using PrimeComm.Properties;
+using ScintillaNET;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace PrimeComm
+{
+    public partial class FormCharmapWindow : DockContent
+    {
+        private readonly FormEditor _parent;
+
+        public FormCharmapWindow(FormEditor parent, FontFamily fontFamily)
+        {
+            _parent = parent;
+            InitializeComponent();
+
+            if (fontFamily != null)
+                charmap.Font = new Font(fontFamily, (int) Settings.Default.EditorFontSize);
+        }
+    }
+}
