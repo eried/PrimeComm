2a1f7ff501a9dddaa470301e5c427b2b901afa3b
Erwin Ried <erwinried@gmail.com>
2014-01-25 12:51:27 -0300

    Setup script

---
+++ Setup/Make.cmd
@@ -0,0 +1,12 @@
+rem Clean output
+rmdir /s /q files
+mkdir files
+
+rem Copy PrimeComm files
+for /R ..\PrimeComm\bin\Debug %%f in (*.exe) do copy %%f files
+for /R ..\PrimeComm\bin\Debug %%f in (*.ttf) do copy %%f files
+for /R ..\PrimeComm\bin\Debug %%f in (*.dll) do copy %%f files
+for /R ..\PrimeComm\bin\Debug %%f in (*.exe) do copy %%f files
+del /s "files\*.vshost.exe"
+
+rem Build the Installer
