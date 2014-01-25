ac70d88689d3549bdb4162a26449155ded47fd75
Erwin Ried <erwinried@gmail.com>
2014-01-25 17:44:38 -0300

    New script for the setup

--- Setup/Make.cmd
+++ Setup/Make.cmd
@@ -10,3 +10,10 @@ for /R ..\PrimeComm\bin\Debug %%f in (*.exe) do copy %%f files
 del /s "files\*.vshost.exe"
 
 rem Build the Installer
+iscc setup.iss
+pause
+
+rem Open the folder containing the setup
+cd setup
+explorer %cd%
+exit
