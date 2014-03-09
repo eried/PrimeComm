210583bcd0f6d5364a08636549fc4a45a0925351
Erwin Ried <erwinried@gmail.com>
2014-03-09 19:14:36 -0400

    Adding Save, Load, Change image and other functions to PrimeSkin

--- PrimeSkin/FormMain.Designer.cs
+++ PrimeSkin/FormMain.Designer.cs
@@ -34,16 +34,16 @@
             this.buttonSave = new System.Windows.Forms.Button();
             this.buttonLoad = new System.Windows.Forms.Button();
             this.panelSkin = new System.Windows.Forms.Panel();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBoxComponents = new System.Windows.Forms.GroupBox();
             this.propertyGridComponent = new System.Windows.Forms.PropertyGrid();
             this.comboBoxSelection = new System.Windows.Forms.ComboBox();
             this.groupBoxVisuals = new System.Windows.Forms.GroupBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.buttonBackground = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonRebuildBorder = new System.Windows.Forms.Button();
             this.groupBoxSkin.SuspendLayout();
-            this.groupBox1.SuspendLayout();
+            this.groupBoxComponents.SuspendLayout();
             this.groupBoxVisuals.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -61,31 +61,37 @@
             // 
             // buttonSaveAs
             // 
-            this.buttonSaveAs.Enabled = false;
-            this.buttonSaveAs.Location = new System.Drawing.Point(180, 21);
+            this.buttonSaveAs.Location = new System.Drawing.Point(180, 19);
             this.buttonSaveAs.Name = "buttonSaveAs";
-            this.buttonSaveAs.Size = new System.Drawing.Size(75, 23);
+            this.buttonSaveAs.Size = new System.Drawing.Size(75, 28);
             this.buttonSaveAs.TabIndex = 2;
             this.buttonSaveAs.Text = "&Save as...";
             this.buttonSaveAs.UseVisualStyleBackColor = true;
+            this.buttonSaveAs.Click += new System.EventHandler(this.buttonSaveAs_Click);
             // 
             // buttonSave
             // 
-            this.buttonSave.Enabled = false;
-            this.buttonSave.Location = new System.Drawing.Point(99, 21);
+            this.buttonSave.Image = ((System.Drawing.Image)(resources.GetObject("buttonSave.Image")));
+            this.buttonSave.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonSave.Location = new System.Drawing.Point(99, 19);
             this.buttonSave.Name = "buttonSave";
-            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.Size = new System.Drawing.Size(75, 28);
             this.buttonSave.TabIndex = 1;
             this.buttonSave.Text = "&Save";
+            this.buttonSave.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
             // 
             // buttonLoad
             // 
-            this.buttonLoad.Location = new System.Drawing.Point(18, 21);
+            this.buttonLoad.Image = ((System.Drawing.Image)(resources.GetObject("buttonLoad.Image")));
+            this.buttonLoad.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonLoad.Location = new System.Drawing.Point(18, 19);
             this.buttonLoad.Name = "buttonLoad";
-            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.Size = new System.Drawing.Size(75, 28);
             this.buttonLoad.TabIndex = 0;
             this.buttonLoad.Text = "&Load";
+            this.buttonLoad.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.buttonLoad.UseVisualStyleBackColor = true;
             this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
             // 
@@ -103,18 +109,18 @@
             this.panelSkin.Size = new System.Drawing.Size(438, 747);
             this.panelSkin.TabIndex = 3;
             // 
-            // groupBox1
+            // groupBoxComponents
             // 
-            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.groupBoxComponents.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-            this.groupBox1.Controls.Add(this.propertyGridComponent);
-            this.groupBox1.Controls.Add(this.comboBoxSelection);
-            this.groupBox1.Location = new System.Drawing.Point(12, 240);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(275, 519);
-            this.groupBox1.TabIndex = 2;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Components";
+            this.groupBoxComponents.Controls.Add(this.propertyGridComponent);
+            this.groupBoxComponents.Controls.Add(this.comboBoxSelection);
+            this.groupBoxComponents.Location = new System.Drawing.Point(12, 240);
+            this.groupBoxComponents.Name = "groupBoxComponents";
+            this.groupBoxComponents.Size = new System.Drawing.Size(275, 519);
+            this.groupBoxComponents.TabIndex = 2;
+            this.groupBoxComponents.TabStop = false;
+            this.groupBoxComponents.Text = "Components";
             // 
             // propertyGridComponent
             // 
@@ -122,8 +128,9 @@
             | System.Windows.Forms.AnchorStyles.Left)));
             this.propertyGridComponent.Location = new System.Drawing.Point(18, 46);
             this.propertyGridComponent.Name = "propertyGridComponent";
-            this.propertyGridComponent.Size = new System.Drawing.Size(237, 467);
+            this.propertyGridComponent.Size = new System.Drawing.Size(237, 456);
             this.propertyGridComponent.TabIndex = 1;
+            this.propertyGridComponent.PropertyValueChanged += new System.Windows.Forms.PropertyValueChangedEventHandler(this.propertyGridComponent_PropertyValueChanged);
             // 
             // comboBoxSelection
             // 
@@ -137,11 +144,10 @@
             // 
             // groupBoxVisuals
             // 
-            this.groupBoxVisuals.Controls.Add(this.checkBox1);
             this.groupBoxVisuals.Controls.Add(this.buttonBackground);
             this.groupBoxVisuals.Controls.Add(this.label1);
-            this.groupBoxVisuals.Controls.Add(this.button4);
-            this.groupBoxVisuals.Enabled = false;
+            this.groupBoxVisuals.Controls.Add(this.button1);
+            this.groupBoxVisuals.Controls.Add(this.buttonRebuildBorder);
             this.groupBoxVisuals.Location = new System.Drawing.Point(12, 77);
             this.groupBoxVisuals.Name = "groupBoxVisuals";
             this.groupBoxVisuals.Size = new System.Drawing.Size(275, 157);
@@ -149,42 +155,48 @@
             this.groupBoxVisuals.TabStop = false;
             this.groupBoxVisuals.Text = "Visuals";
             // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(19, 48);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(208, 17);
-            this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "Copy the background near the skin file";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
             // buttonBackground
             // 
+            this.buttonBackground.Image = ((System.Drawing.Image)(resources.GetObject("buttonBackground.Image")));
+            this.buttonBackground.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.buttonBackground.Location = new System.Drawing.Point(18, 19);
             this.buttonBackground.Name = "buttonBackground";
-            this.buttonBackground.Size = new System.Drawing.Size(237, 23);
+            this.buttonBackground.Size = new System.Drawing.Size(237, 28);
             this.buttonBackground.TabIndex = 0;
             this.buttonBackground.Text = "&Change background...";
+            this.buttonBackground.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.buttonBackground.UseVisualStyleBackColor = true;
+            this.buttonBackground.Click += new System.EventHandler(this.buttonBackground_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 102);
+            this.label1.Enabled = false;
+            this.label1.Location = new System.Drawing.Point(15, 99);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(167, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Border used for chromeless mode:";
             // 
-            // button4
+            // button1
             // 
-            this.button4.Location = new System.Drawing.Point(18, 118);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(237, 23);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "&Rebuild border from transparency";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(180, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "&Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // buttonRebuildBorder
+            // 
+            this.buttonRebuildBorder.Enabled = false;
+            this.buttonRebuildBorder.Location = new System.Drawing.Point(18, 115);
+            this.buttonRebuildBorder.Name = "buttonRebuildBorder";
+            this.buttonRebuildBorder.Size = new System.Drawing.Size(156, 28);
+            this.buttonRebuildBorder.TabIndex = 3;
+            this.buttonRebuildBorder.Text = "&Rebuild from background";
+            this.buttonRebuildBorder.UseVisualStyleBackColor = true;
             // 
             // FormMain
             // 
@@ -192,15 +204,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(743, 771);
             this.Controls.Add(this.groupBoxVisuals);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBoxComponents);
             this.Controls.Add(this.panelSkin);
             this.Controls.Add(this.groupBoxSkin);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(580, 480);
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
             this.groupBoxSkin.ResumeLayout(false);
-            this.groupBox1.ResumeLayout(false);
+            this.groupBoxComponents.ResumeLayout(false);
             this.groupBoxVisuals.ResumeLayout(false);
             this.groupBoxVisuals.PerformLayout();
             this.ResumeLayout(false);
@@ -214,14 +227,14 @@
         private System.Windows.Forms.Button buttonSaveAs;
         private System.Windows.Forms.Button buttonSave;
         private System.Windows.Forms.Button buttonLoad;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBoxComponents;
         private System.Windows.Forms.PropertyGrid propertyGridComponent;
         private System.Windows.Forms.ComboBox comboBoxSelection;
         private System.Windows.Forms.GroupBox groupBoxVisuals;
-        private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Button buttonBackground;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button buttonRebuildBorder;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- PrimeSkin/FormMain.cs
+++ PrimeSkin/FormMain.cs
@@ -6,37 +6,68 @@ using System.IO;
 using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
+using PrimeSkin.Properties;
 
 namespace PrimeSkin
 {
     public partial class FormMain : Form
     {
         private Skin _currentSkin;
+        private bool _dirty;
+        private readonly string _initialDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), @"Hewlett-Packard\HP Prime Virtual Calculator\");
 
         public FormMain()
         {
+            InitializeComponent();
+            UpdateGui();
+
+            // Initial window title includes the version
             var v = Assembly.GetExecutingAssembly().GetName().Version;
             Text = String.Format("{0} v{1} b{2}", Application.ProductName, v.ToString(2), v.Build);
+        }
 
-            InitializeComponent();
+        private void UpdateGui()
+        {
+            var isSkinLoaded = _currentSkin != null;
+            
+            panelSkin.Enabled = isSkinLoaded;
+            groupBoxComponents.Enabled = isSkinLoaded;
+            groupBoxVisuals.Enabled = isSkinLoaded;
+            buttonSave.Enabled = isSkinLoaded;
+            buttonSaveAs.Enabled = isSkinLoaded;
+
+            Text = (_dirty?"* ":"")+(isSkinLoaded ? Path.GetFileNameWithoutExtension(_currentSkin.SkinPath) + ": " : String.Empty) + Application.ProductName;
         }
 
         private void LoadSkin(string path)
         {
-            panelSkin.Controls.Clear();
-            var pictureBoxSkin = new PictureBox
+            if (AskForSave())
             {
-                Parent = panelSkin,
-                Location = new Point(panelSkin.Margin.Left, panelSkin.Margin.Right),
-                Anchor = AnchorStyles.Left | AnchorStyles.Top
-            };
-            _currentSkin = new Skin(path, pictureBoxSkin);
+                try
+                {
+                    panelSkin.Controls.Clear();
+                    var pictureBoxSkin = new PictureBox
+                    {
+                        Parent = panelSkin,
+                        Location = new Point(panelSkin.Margin.Left, panelSkin.Margin.Right),
+                        Anchor = AnchorStyles.Left | AnchorStyles.Top
+                    };
+                    _currentSkin = new Skin(path, pictureBoxSkin);
 
-            comboBoxSelection.Items.Clear();
-            comboBoxSelection.Items.AddRange(_currentSkin.Components.ToArray());
-            
-            _currentSkin.SelectedComponentChange += _currentSkin_SelectedComponentChange;
-            _currentSkin.Selected = null;
+                    comboBoxSelection.Items.Clear();
+                    comboBoxSelection.Items.AddRange(_currentSkin.Components.ToArray());
+
+                    _currentSkin.SelectedComponentChange += _currentSkin_SelectedComponentChange;
+                    _currentSkin.Selected = null;
+                    _dirty = false;
+                }
+                catch
+                {
+                    _currentSkin = null;
+                    MessageBox.Show("Error loading the skin", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            UpdateGui();
         }
 
         void _currentSkin_SelectedComponentChange(object sender, SelectedComponentEventArgs e)
@@ -49,8 +80,8 @@ namespace PrimeSkin
         {
             var f = new OpenFileDialog
             {
-                Filter = "Skin file (*.skin)|*.skin",
-                InitialDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), @"Hewlett-Packard\HP Prime Virtual Calculator\")
+                Filter = Resources.FilterInput,
+                InitialDirectory = _initialDirectory
             };
 
             if (f.ShowDialog() == DialogResult.OK)
@@ -61,7 +92,6 @@ namespace PrimeSkin
                 catch
                 {
                 }
-
         }
 
         private void comboBoxSelection_SelectedIndexChanged(object sender, EventArgs e)
@@ -70,8 +100,91 @@ namespace PrimeSkin
             {
                 _currentSkin.Selected = comboBoxSelection.SelectedItem as Component;
                 propertyGridComponent.SelectedObject = _currentSkin.Selected;
+
                 propertyGridComponent.ExpandAllGridItems();
             }
+        }
+
+        private void propertyGridComponent_PropertyValueChanged(object s, PropertyValueChangedEventArgs e)
+        {
+            _currentSkin.Refresh(true);
+
+            _dirty = true;
+            UpdateGui();
+        }
+
+        private bool AskForSave()
+        {
+            if (_dirty)
+            {
+                switch (
+                    MessageBox.Show(String.Format("Save the changes to '{0}'?", Path.GetFileNameWithoutExtension(_currentSkin.SkinPath)), "Save changes",
+                        MessageBoxButtons.YesNoCancel,
+                        MessageBoxIcon.Exclamation))
+                {
+                    case DialogResult.Yes:
+                        if (_currentSkin.Save())
+                            _dirty = false;
+                        break;
+                    case DialogResult.No:
+                        _dirty = false;
+                        break;
+                    default:
+                        return false; // Cancel 
+                }
+            }
+            return true;
+        }
+
+        private void buttonBackground_Click(object sender, EventArgs e)
+        {
+            var f = new OpenFileDialog
+            {
+                Filter = "Bitmap (*.bmp)|*.bmp",
+                InitialDirectory = _currentSkin.BasePath
+            };
+
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                _currentSkin.ImagePath = f.FileName;
+                _currentSkin.Refresh(true);
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        private void Save(String path="")
+        {
+            if (_currentSkin.Save(path))
+            {
+                _dirty = false;
+
+                LoadSkin(String.IsNullOrEmpty(path) ? _currentSkin.SkinPath : path);
+            }
+            else
+                MessageBox.Show("Error saving the skin (check if you have privileges in the destination folder and retry)", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void buttonSaveAs_Click(object sender, EventArgs e)
+        {
+            var f = new SaveFileDialog
+            {
+                Filter = Resources.FilterInput,
+                InitialDirectory = _initialDirectory
+            };
+
+            if (f.ShowDialog() == DialogResult.OK)
+                Save(f.FileName);
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!AskForSave())
+                e.Cancel = true;
         }
     }
 }
