139f86d213624881ef11de74517e58da461915d3
Erwin Ried <erwinried@gmail.com>
2014-03-19 21:14:33 -0300

    Lock Screen aspect ratio

--- PrimeSkin/VirtualScreen.cs
+++ PrimeSkin/VirtualScreen.cs
@@ -1,4 +1,6 @@
 using System;
+using System.ComponentModel;
+using System.Drawing;
 
 namespace PrimeSkin
 {
@@ -8,14 +10,41 @@ namespace PrimeSkin
     [Serializable]
     public class VirtualScreen : VirtualComponent
     {
+        private Rectangle _rectangle;
+
         public VirtualScreen()
         {
             Type = ComponentType.Screen;
+            LockRatio = true;
         }
 
         public override string ToString()
         {
             return "Screen";
+        }
+
+        [Category("Layout"), Description("Calculates the height of the screen to match the required 4:3 proportion of the emulator")]
+        public bool LockRatio { get; set; }
+
+        internal override void RecalculateLayout(Rectangle bounds)
+        {
+            // Do base calculations first
+            base.RecalculateLayout(bounds);
+
+            if (!LockRatio)
+                return;
+
+            // Adjust to 4:3 restriction
+            int w = Rectangle.Width/4, h = Rectangle.Height/3;
+
+            if(w==h)
+                return;
+
+            var s = new Rectangle(Rectangle.Location,new Size(Rectangle.Width, w * 3));
+            if (s.Bottom <= bounds.Bottom && s.Right <= bounds.Right)
+                Rectangle = s;
+            else
+                Rectangle = new Rectangle(Rectangle.Location, new Size(h * 4, Rectangle.Height));
         }
     }
 }
