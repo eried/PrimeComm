19a795f517e1e0d500ee117f7152f0bb7efed26f
Erwin Ried <erwinried@gmail.com>
2014-03-31 15:05:27 -0300

    Extra step in Make.cmd was not needed

--- Setup/Make.cmd
+++ Setup/Make.cmd
@@ -6,7 +6,6 @@ rem Copy PrimeComm files
 for /R ..\PrimeComm\bin\Debug %%f in (*.exe) do copy %%f files
 for /R ..\PrimeComm\bin\Debug %%f in (*.ttf) do copy %%f files
 for /R ..\PrimeComm\bin\Debug %%f in (*.dll) do copy %%f files
-for /R ..\PrimeComm\bin\Debug %%f in (*.exe) do copy %%f files
 del /s "files\*.vshost.exe"
 
 rem Build the Installer
