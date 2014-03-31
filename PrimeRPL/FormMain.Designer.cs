f6100283b3236fb0a6bb870be7d1131b83655084
Erwin Ried <erwinried@gmail.com>
2014-03-31 14:46:23 -0300

    Early PrimeRPL version

---
+++ PrimeRPL/FormMain.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace PrimeRPL
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.editor = new System.Windows.Forms.TextBox();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.textBoxPreview = new System.Windows.Forms.TextBox();
+            this.buttonPreview = new System.Windows.Forms.Button();
+            this.buttonPreviewAndCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // editor
+            // 
+            this.editor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.editor.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editor.Location = new System.Drawing.Point(12, 12);
+            this.editor.Multiline = true;
+            this.editor.Name = "editor";
+            this.editor.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.editor.Size = new System.Drawing.Size(618, 271);
+            this.editor.TabIndex = 0;
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonConvert.Location = new System.Drawing.Point(555, 289);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(75, 23);
+            this.buttonConvert.TabIndex = 1;
+            this.buttonConvert.Text = "Convert...";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSave.Location = new System.Drawing.Point(474, 289);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 1;
+            this.buttonSave.Text = "Save...";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // textBoxPreview
+            // 
+            this.textBoxPreview.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxPreview.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPreview.Location = new System.Drawing.Point(12, 318);
+            this.textBoxPreview.Multiline = true;
+            this.textBoxPreview.Name = "textBoxPreview";
+            this.textBoxPreview.ReadOnly = true;
+            this.textBoxPreview.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxPreview.Size = new System.Drawing.Size(618, 248);
+            this.textBoxPreview.TabIndex = 2;
+            // 
+            // buttonPreview
+            // 
+            this.buttonPreview.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonPreview.Location = new System.Drawing.Point(19, 289);
+            this.buttonPreview.Name = "buttonPreview";
+            this.buttonPreview.Size = new System.Drawing.Size(75, 23);
+            this.buttonPreview.TabIndex = 3;
+            this.buttonPreview.Text = "Preview";
+            this.buttonPreview.UseVisualStyleBackColor = true;
+            this.buttonPreview.Click += new System.EventHandler(this.buttonPreview_Click);
+            // 
+            // buttonPreviewAndCopy
+            // 
+            this.buttonPreviewAndCopy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonPreviewAndCopy.Location = new System.Drawing.Point(100, 289);
+            this.buttonPreviewAndCopy.Name = "buttonPreviewAndCopy";
+            this.buttonPreviewAndCopy.Size = new System.Drawing.Size(123, 23);
+            this.buttonPreviewAndCopy.TabIndex = 3;
+            this.buttonPreviewAndCopy.Text = "Preview and copy";
+            this.buttonPreviewAndCopy.UseVisualStyleBackColor = true;
+            this.buttonPreviewAndCopy.Click += new System.EventHandler(this.buttonPreviewAndCopy_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 578);
+            this.Controls.Add(this.buttonPreviewAndCopy);
+            this.Controls.Add(this.buttonPreview);
+            this.Controls.Add(this.textBoxPreview);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonConvert);
+            this.Controls.Add(this.editor);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.Text = "PrimeRPL";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox editor;
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.TextBox textBoxPreview;
+        private System.Windows.Forms.Button buttonPreview;
+        private System.Windows.Forms.Button buttonPreviewAndCopy;
+    }
+}
+
---
+++ PrimeRPL/FormMain.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using PrimeLib;
+using PrimeRPL.Properties;
+
+namespace PrimeRPL
+{
+    public partial class FormMain : Form
+    {
+        private PrimeLanguageConverter _converter;
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            // Initialize the converter
+            _converter = new PrimeLanguageConverter
+            {
+                Header =
+                    "// File created by " + Application.ProductName + Environment.NewLine +
+                    "// Part of PrimeComm: http://servicios.ried.cl/primecomm/" + Environment.NewLine 
+            };
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            var f = new OpenFileDialog {Filter = "txt file|.txt"};
+
+            if (f.ShowDialog() == DialogResult.OK)
+                File.WriteAllText(f.FileName, editor.Text);
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            var f = new OpenFileDialog {Filter = "hpprgm file|.hpprgm"};
+
+            if (f.ShowDialog() == DialogResult.OK)
+                new PrimeUsbData(null, Encoding.Unicode.GetBytes(_converter.Convert(editor.Text))).Save(f.FileName);
+        }
+
+        private void buttonPreview_Click(object sender, EventArgs e)
+        {
+            GeneratePreview();
+        }
+
+        private void GeneratePreview()
+        {
+            textBoxPreview.Text = _converter.Convert(editor.Text);
+            textBoxPreview.SelectionLength = 0;
+            textBoxPreview.SelectionStart = textBoxPreview.TextLength - 1;
+            textBoxPreview.ScrollToCaret();
+        }
+
+        private void buttonPreviewAndCopy_Click(object sender, EventArgs e)
+        {
+            GeneratePreview();
+            Clipboard.SetText(textBoxPreview.Text);
+        }
+    }
+
+    public class PrimeLanguageConverter
+    {
+        private readonly Regex regexStrings, regexComments;
+        private const string EncodePrefix = "____[", EncodePostfix = "]____";
+
+        public PrimeLanguageConverter()
+        {
+            regexStrings = new Regex(@"""[^""""\\]*(?:\\.[^""""\\]*)*""");
+            regexComments = new Regex(@"^\s*.*@.*", RegexOptions.Multiline);
+        }
+
+        /// <summary>
+        /// This converts RPL code into a code compatible with the Prime calculator language (HPPPL)
+        /// </summary>
+        /// <param name="programCode">RPL code source</param>
+        /// <returns>HPPPL code</returns>
+        public string Convert(string programCode)
+        {
+            var r = new StringBuilder(Header);
+
+            // Remove comments and encode strings
+            programCode = regexStrings.Replace(regexComments.Replace(programCode, String.Empty), EncodeElement);
+
+            // Remove empty lines and spaces
+            programCode = programCode.Replace("\r\n", "\n").Replace("\n\r", "\n").Replace("\r", "\n").Replace("\n", " ").Replace("  ", " ").Replace("  ", " ");
+            
+            // Add default program code
+            var token = "sz";
+            
+            // Process code
+            var sentences = new List<String>();
+            foreach (var b in programCode.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries))
+            {
+                switch (b.ToUpper())
+                {
+                    case "+":
+                    case "-":
+                    case "/":
+                    case "*":
+                    case "^":
+                    case "%":
+                    case "MOD":
+                        sentences.Add(token + "evaluateAsOperator(\"" + b + "\");"); // Operators
+                        break;
+
+                    case "ASC":
+                    case "CHAR":
+                    case "DIM":
+                    case "TYPE":
+                        sentences.Add(token + "evaluateAsFunction(\"" + b + "\",1,false);"); // Functions with 1 param
+                        break;
+
+                    case "MSGBOX":
+                    case "PRINT":
+                        sentences.Add(token + "evaluateAsFunction(\"" + b + "\",1,true);"); // Functions with 1 param, void
+                        break;
+
+                    case "INSTRING":
+                    case "LEFT":
+                    case "RIGHT":
+                    case "CONCAT":
+                        sentences.Add(token + "evaluateAsFunction(\"" + b + "\",2,false);"); // Functions with 2 param
+                        break;
+
+                    case "REPLACE":
+                        sentences.Add(token + "evaluateAsFunction(\"" + b + "\",3,false);"); // Functions with 3 param
+                        break;
+
+                    case "->STR":
+                        sentences.Add(token + "evaluateAsFunction(\"STRING\",1,false);"); // Functions with 1 param
+                        break;
+
+                    case "SWAP":
+                        sentences.Add(token + "swap();"); // Functions with 1 param
+                        break;
+
+                    case "DUP":
+                        sentences.Add(token + "dup();"); // Functions with 1 param
+                        break;
+
+                    case "DROP":
+                        sentences.Add(token + "pop();"); // Functions with 1 param
+                        break;
+
+                    default:
+                    {
+                        bool shouldPush;
+                        string toPush = b;
+
+                        // Check for strings
+                        if (b.StartsWith(EncodePrefix) && b.EndsWith(EncodePostfix))
+                        {
+                            toPush = DecodeString(b.Substring(EncodePrefix.Length, b.Length - EncodePrefix.Length - EncodePostfix.Length));
+                            shouldPush = true;
+                        }
+                        else
+                        {
+                            double t;
+                            shouldPush = double.TryParse(b, out t);
+                        }
+
+                        if(shouldPush)
+                            sentences.Add(token + "push(" + toPush + ");");
+                    }
+                        break;
+                }
+            }
+
+            r.AppendLine(Resources.DefaultProgram.Replace("{token}", token).Replace("{code}",String.Join(" ",sentences)));
+
+            r.AppendLine(Footer);
+            return r.ToString();
+        }
+
+
+        private static string DecodeElement(Match match)
+        {
+            try
+            {
+                return DecodeString(match.Groups["data"].Value);
+            }
+            catch
+            {
+            }
+            return null;
+        }
+
+        private static string DecodeString(string value)
+        {
+            return Encoding.Unicode.GetString(System.Convert.FromBase64String(value));
+        }
+
+        private static string EncodeElement(Match match)
+        {
+            return EncodePrefix + System.Convert.ToBase64String(Encoding.Unicode.GetBytes(match.Value)) + EncodePostfix;
+        }
+
+        public string Header { get; set; }
+
+        public string Footer { get; set; }
+    }
+}
