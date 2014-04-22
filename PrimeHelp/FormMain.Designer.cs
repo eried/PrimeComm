aeeafe60ff53275b49320123f8198b4a88872a72
Erwin Ried <erwinried@gmail.com>
2014-04-22 18:09:43 -0300

    PrimeHelp v1.0

---
+++ PrimeHelp/FormMain.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace PrimeHelp
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.listBoxTerms = new System.Windows.Forms.ListBox();
+            this.textBoxView = new System.Windows.Forms.TextBox();
+            this.backgroundWorkerLoad = new System.ComponentModel.BackgroundWorker();
+            this.timerSearch = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorkerSearch = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(13, 13);
+            this.textBoxSearch.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(241, 25);
+            this.textBoxSearch.TabIndex = 0;
+            this.textBoxSearch.TextChanged += new System.EventHandler(this.textBoxSearch_TextChanged);
+            this.textBoxSearch.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBoxSearch_KeyUp);
+            // 
+            // listBoxTerms
+            // 
+            this.listBoxTerms.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBoxTerms.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.listBoxTerms.FormattingEnabled = true;
+            this.listBoxTerms.IntegralHeight = false;
+            this.listBoxTerms.ItemHeight = 18;
+            this.listBoxTerms.Location = new System.Drawing.Point(13, 46);
+            this.listBoxTerms.Margin = new System.Windows.Forms.Padding(4);
+            this.listBoxTerms.Name = "listBoxTerms";
+            this.listBoxTerms.Size = new System.Drawing.Size(241, 438);
+            this.listBoxTerms.TabIndex = 1;
+            this.listBoxTerms.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBoxTerms_DrawItem);
+            this.listBoxTerms.SelectedValueChanged += new System.EventHandler(this.listBoxTerms_SelectedValueChanged);
+            this.listBoxTerms.KeyUp += new System.Windows.Forms.KeyEventHandler(this.listBoxTerms_KeyUp);
+            // 
+            // textBoxView
+            // 
+            this.textBoxView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxView.Location = new System.Drawing.Point(262, 13);
+            this.textBoxView.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxView.Multiline = true;
+            this.textBoxView.Name = "textBoxView";
+            this.textBoxView.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxView.Size = new System.Drawing.Size(427, 471);
+            this.textBoxView.TabIndex = 2;
+            this.textBoxView.KeyUp += new System.Windows.Forms.KeyEventHandler(this.listBoxTerms_KeyUp);
+            // 
+            // backgroundWorkerLoad
+            // 
+            this.backgroundWorkerLoad.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerLoad_DoWork);
+            this.backgroundWorkerLoad.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorkerLoad_RunWorkerCompleted);
+            // 
+            // timerSearch
+            // 
+            this.timerSearch.Tick += new System.EventHandler(this.timerSearch_Tick);
+            // 
+            // backgroundWorkerSearch
+            // 
+            this.backgroundWorkerSearch.WorkerSupportsCancellation = true;
+            this.backgroundWorkerSearch.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerSearch_DoWork);
+            this.backgroundWorkerSearch.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorkerSearch_RunWorkerCompleted);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 497);
+            this.Controls.Add(this.textBoxView);
+            this.Controls.Add(this.listBoxTerms);
+            this.Controls.Add(this.textBoxSearch);
+            this.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PrimeHelp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.ListBox listBoxTerms;
+        private System.Windows.Forms.TextBox textBoxView;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerLoad;
+        private System.Windows.Forms.Timer timerSearch;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerSearch;
+    }
+}
+
---
+++ PrimeHelp/FormMain.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using PrimeHelp.Properties;
+
+namespace PrimeHelp
+{
+    public partial class FormMain : Form
+    {
+        private readonly List<ReferenceDefinition> _reference;
+        private bool _searchAgain;
+
+        public FormMain()
+        {
+            Environment.CurrentDirectory = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+            InitializeComponent();
+
+            _reference = new List<ReferenceDefinition>();
+
+            if(File.Exists(Resources.ReferenceFile))
+                backgroundWorkerLoad.RunWorkerAsync(File.ReadAllText(Resources.ReferenceFile, Encoding.Default));
+        }
+
+        private void backgroundWorkerLoad_DoWork(object sender, DoWorkEventArgs e)
+        {
+            using (var r = new CsvFileReader(new MemoryStream(Encoding.UTF8.GetBytes(e.Argument as string ?? "")),
+                EmptyLineBehavior.EndOfFile))
+            {
+                r.Delimiter = ';';
+
+                var t = new List<String>();
+                while (r.ReadRow(t))
+                {
+                    if (t.Count > 1)
+                    {
+                        if (String.IsNullOrEmpty(t[0]))
+                            break;
+                        _reference.Add(new ReferenceDefinition { Command = t[0], Description = t[1] });
+                    }
+                    else
+                        break;
+                }
+            }
+        }
+
+        private void backgroundWorkerLoad_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            ResetCommandList();
+        }
+
+        private void ResetCommandList()
+        {
+            listBoxTerms.SuspendDrawing();
+            listBoxTerms.Items.Clear();
+
+            foreach (var r in _reference)
+            {
+                r.Bold = false;
+                r.Italic = false;
+                listBoxTerms.Items.Add(r);
+            }
+
+            listBoxTerms.ResumeDrawing();
+        }
+
+        private void listBoxTerms_SelectedValueChanged(object sender, EventArgs e)
+        {
+            var r = listBoxTerms.SelectedItem as ReferenceDefinition;
+
+            if (r != null)
+                textBoxView.Text = r.Description;
+        }
+
+        private void textBoxSearch_TextChanged(object sender, EventArgs e)
+        {
+            timerSearch.Stop();
+            timerSearch.Start();
+        }
+
+        private void timerSearch_Tick(object sender, EventArgs e)
+        {
+            _searchAgain = true;
+
+            if (backgroundWorkerSearch.IsBusy)
+            {
+                if(!backgroundWorkerSearch.CancellationPending)
+                    backgroundWorkerSearch.CancelAsync();
+            }
+            else
+            {
+                backgroundWorkerSearch.RunWorkerAsync(textBoxSearch.Text);
+                timerSearch.Stop();
+            }
+        }
+
+        private void backgroundWorkerSearch_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var searchString = e.Argument as String;
+            var results = new Dictionary<String, ReferenceDefinition>();
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                searchString = searchString.ToLower();
+                //backgroundWorkerSearch.ReportProgress(0);
+
+                // Start with
+                foreach (var r in _reference)
+                {
+                    if (r.Command.StartsWith(searchString, StringComparison.OrdinalIgnoreCase))
+                        if (!results.ContainsKey(r.Command))
+                        {
+                            r.Bold = true;
+                            r.Italic = false;
+                            results.Add(r.Command, r);
+                        }
+                }
+
+                // Contains
+                foreach (var r in _reference)
+                {
+                    if (r.Command.ToLower().Contains(searchString))
+                        if (!results.ContainsKey(r.Command))
+                        {
+                            r.Bold = false;
+                            r.Italic = false;
+                            results.Add(r.Command, r);
+                        }
+                }
+
+                // Content
+                if(searchString.Length > 1)
+                    foreach (var r in _reference)
+                    {
+                        if (r.Description.ToLower().Contains(searchString))
+                            if (!results.ContainsKey(r.Command))
+                            {
+                                r.Bold = false;
+                                r.Italic = true;
+                                results.Add(r.Command, r);
+                            }
+                    }
+            }
+
+            e.Result = results;
+        }
+
+        private void AppendResults(Dictionary<String, ReferenceDefinition> results)
+        {
+            if (results == null) return;
+
+            listBoxTerms.SuspendDrawing();
+            listBoxTerms.Items.Clear();
+            foreach (var r in results)
+                listBoxTerms.Items.Add(r.Value);
+            listBoxTerms.ResumeDrawing();
+        }
+
+        private void backgroundWorkerSearch_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            var r = e.Result as Dictionary<String, ReferenceDefinition>;
+
+            if (r.Count == 0)
+                ResetCommandList();
+            else
+            {
+                AppendResults(r);
+            }
+        }
+
+        private void listBoxTerms_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            e.DrawBackground();
+            var s = listBoxTerms.Items[e.Index] as ReferenceDefinition;
+
+            if (s != null)
+            {
+                var style = FontStyle.Regular;
+
+                if (s.Bold)
+                    style |= FontStyle.Bold;
+
+                if (s.Italic)
+                    style |= FontStyle.Italic;
+
+                e.Graphics.DrawString(s.ToString(), new Font(Font.FontFamily, Font.Size, style), Brushes.Black, e.Bounds);
+            }
+            e.DrawFocusRectangle();
+        }
+
+        private void textBoxSearch_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyValue)
+            {
+                case 40:
+                case 38:
+                case 13:
+                    if (listBoxTerms.Items.Count > 0)
+                    {
+                        listBoxTerms.Select();
+                        listBoxTerms.SelectedIndex = 0;
+                    }
+                    break;
+            }
+        }
+
+        private void listBoxTerms_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == 13)
+            {
+                textBoxSearch.SelectAll();
+                textBoxSearch.Select();
+            }
+        }
+    }
+
+    /// <summary>
+    /// http://stackoverflow.com/questions/487661/how-do-i-suspend-painting-for-a-control-and-its-children
+    /// </summary>
+    static class DrawingControl
+    {
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, Int32 wMsg, bool wParam, Int32 lParam);
+
+        private const int WM_SETREDRAW = 11;
+
+        public static void SuspendDrawing(this Control parent)
+        {
+            SendMessage(parent.Handle, WM_SETREDRAW, false, 0);
+        }
+
+        public static void ResumeDrawing(this Control parent)
+        {
+            SendMessage(parent.Handle, WM_SETREDRAW, true, 0);
+            parent.Refresh();
+        }
+    }
+
+    internal class ReferenceDefinition
+    {
+        public string Command, Description;
+        public bool Bold { get; set; }
+        public bool Italic { get; set; }
+
+        public override string ToString()
+        {
+            return Command;
+        }
+    }
+}
