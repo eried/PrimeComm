8f0f59b9b069ab2f716aa22a59c254c7026fbc89
Erwin Ried <erwinried@gmail.com>
2014-04-23 12:58:27 -0300

    PrimeComm v0.9 b21

--- PrimeHelp/Properties/AssemblyInfo.cs
+++ PrimeHelp/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PrimeHelp")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Erwin Ried 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
