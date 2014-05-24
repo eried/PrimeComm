decc8950ebf13d38636dbaad03e113b4945bb29b
Erwin Ried <erwinried@gmail.com>
2014-05-24 17:27:51 -0400

    More housekeeping

--- PrimeComm/FormEditor.Designer.cs
+++ PrimeComm/FormEditor.Designer.cs
@@ -253,6 +253,7 @@ namespace PrimeComm
             dockPaneStripToolWindowGradient1.InactiveTabGradient = tabGradient7;
             dockPaneStripSkin1.ToolWindowGradient = dockPaneStripToolWindowGradient1;
             dockPanelSkin1.DockPaneStripSkin = dockPaneStripSkin1;
+            this.dockPanel.Skin = dockPanelSkin1;
             this.dockPanel.TabIndex = 1;
             // 
             // menuStripMain
