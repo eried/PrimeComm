ce8dcf191bf36fd2a092c708b673e18788e35fa0
Erwin Ried <erwinried@gmail.com>
2017-03-15 10:53:45 +0100

    Program rebuild to use Keyboard library

--- PrimeMon/FormMain.Designer.cs
+++ PrimeMon/FormMain.Designer.cs
@@ -28,23 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.labelDragHere = new System.Windows.Forms.Label();
             this.fileSystemWatcherMonitor = new System.IO.FileSystemWatcher();
             this.checkBoxStep1Select = new System.Windows.Forms.CheckBox();
             this.checkBoxStep2Edit = new System.Windows.Forms.CheckBox();
             this.checkBoxStep3Escape = new System.Windows.Forms.CheckBox();
+            this.timerExecute = new System.Windows.Forms.Timer(this.components);
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonReference = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcherMonitor)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.tableLayoutPanel1.SetColumnSpan(this.groupBox1, 3);
             this.groupBox1.Controls.Add(this.labelDragHere);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.142858F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(651, 131);
-            this.groupBox1.TabIndex = 0;
+            this.groupBox1.Size = new System.Drawing.Size(649, 164);
+            this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Which program to monitor?";
             // 
@@ -54,7 +67,7 @@
             this.labelDragHere.Dock = System.Windows.Forms.DockStyle.Fill;
             this.labelDragHere.Location = new System.Drawing.Point(3, 25);
             this.labelDragHere.Name = "labelDragHere";
-            this.labelDragHere.Size = new System.Drawing.Size(645, 103);
+            this.labelDragHere.Size = new System.Drawing.Size(643, 136);
             this.labelDragHere.TabIndex = 0;
             this.labelDragHere.Text = "Drag a hpprgm file here";
             this.labelDragHere.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -63,6 +76,7 @@
             // 
             // fileSystemWatcherMonitor
             // 
+            this.fileSystemWatcherMonitor.EnableRaisingEvents = true;
             this.fileSystemWatcherMonitor.NotifyFilter = ((System.IO.NotifyFilters)((System.IO.NotifyFilters.FileName | System.IO.NotifyFilters.Size)));
             this.fileSystemWatcherMonitor.SynchronizingObject = this;
             this.fileSystemWatcherMonitor.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcherMonitor_Changed);
@@ -74,10 +88,10 @@
             this.checkBoxStep1Select.Checked = true;
             this.checkBoxStep1Select.CheckState = System.Windows.Forms.CheckState.Checked;
             this.checkBoxStep1Select.Enabled = false;
-            this.checkBoxStep1Select.Location = new System.Drawing.Point(12, 160);
+            this.checkBoxStep1Select.Location = new System.Drawing.Point(33, 40);
             this.checkBoxStep1Select.Name = "checkBoxStep1Select";
             this.checkBoxStep1Select.Size = new System.Drawing.Size(484, 29);
-            this.checkBoxStep1Select.TabIndex = 1;
+            this.checkBoxStep1Select.TabIndex = 0;
             this.checkBoxStep1Select.Text = "Select the modified program in the Program Catalog";
             this.checkBoxStep1Select.UseVisualStyleBackColor = true;
             // 
@@ -86,41 +100,130 @@
             this.checkBoxStep2Edit.AutoSize = true;
             this.checkBoxStep2Edit.Checked = true;
             this.checkBoxStep2Edit.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBoxStep2Edit.Location = new System.Drawing.Point(47, 195);
+            this.checkBoxStep2Edit.Location = new System.Drawing.Point(63, 75);
             this.checkBoxStep2Edit.Name = "checkBoxStep2Edit";
-            this.checkBoxStep2Edit.Size = new System.Drawing.Size(258, 29);
+            this.checkBoxStep2Edit.Size = new System.Drawing.Size(373, 29);
             this.checkBoxStep2Edit.TabIndex = 1;
-            this.checkBoxStep2Edit.Text = "Edit the modified program";
+            this.checkBoxStep2Edit.Text = "E&dit the modified program by opening it";
             this.checkBoxStep2Edit.UseVisualStyleBackColor = true;
+            this.checkBoxStep2Edit.CheckedChanged += new System.EventHandler(this.checkBoxStep2Edit_CheckedChanged);
             // 
             // checkBoxStep3Escape
             // 
             this.checkBoxStep3Escape.AutoSize = true;
             this.checkBoxStep3Escape.Checked = true;
             this.checkBoxStep3Escape.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBoxStep3Escape.Location = new System.Drawing.Point(85, 230);
+            this.checkBoxStep3Escape.Location = new System.Drawing.Point(93, 110);
             this.checkBoxStep3Escape.Name = "checkBoxStep3Escape";
             this.checkBoxStep3Escape.Size = new System.Drawing.Size(512, 29);
-            this.checkBoxStep3Escape.TabIndex = 1;
-            this.checkBoxStep3Escape.Text = "Escape the modified program to invoke checking errors";
+            this.checkBoxStep3Escape.TabIndex = 2;
+            this.checkBoxStep3Escape.Text = "E&scape the modified program to invoke checking errors";
             this.checkBoxStep3Escape.UseVisualStyleBackColor = true;
+            // 
+            // timerExecute
+            // 
+            this.timerExecute.Tick += new System.EventHandler(this.timerExecute_Tick);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonExit.Image = ((System.Drawing.Image)(resources.GetObject("buttonExit.Image")));
+            this.buttonExit.Location = new System.Drawing.Point(568, 361);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(94, 47);
+            this.buttonExit.TabIndex = 1;
+            this.buttonExit.Text = "&Exit";
+            this.buttonExit.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonExit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonEdit.Enabled = false;
+            this.buttonEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.142858F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEdit.Image = ((System.Drawing.Image)(resources.GetObject("buttonEdit.Image")));
+            this.buttonEdit.Location = new System.Drawing.Point(368, 361);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(194, 47);
+            this.buttonEdit.TabIndex = 0;
+            this.buttonEdit.Text = "Edit &Program";
+            this.buttonEdit.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonEdit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // groupBox2
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.groupBox2, 3);
+            this.groupBox2.Controls.Add(this.checkBoxStep1Select);
+            this.groupBox2.Controls.Add(this.checkBoxStep2Edit);
+            this.groupBox2.Controls.Add(this.checkBoxStep3Escape);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(13, 183);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(649, 164);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Actions after the program changes";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.buttonReference, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.buttonEdit, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonExit, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(10);
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 70F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(675, 430);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // buttonReference
+            // 
+            this.buttonReference.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonReference.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.142858F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonReference.Image = ((System.Drawing.Image)(resources.GetObject("buttonReference.Image")));
+            this.buttonReference.Location = new System.Drawing.Point(13, 361);
+            this.buttonReference.Name = "buttonReference";
+            this.buttonReference.Size = new System.Drawing.Size(194, 47);
+            this.buttonReference.TabIndex = 4;
+            this.buttonReference.Text = "&Reference";
+            this.buttonReference.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonReference.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonReference.UseVisualStyleBackColor = true;
+            this.buttonReference.Visible = false;
+            this.buttonReference.Click += new System.EventHandler(this.buttonReference_Click);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(675, 283);
-            this.Controls.Add(this.checkBoxStep3Escape);
-            this.Controls.Add(this.checkBoxStep2Edit);
-            this.Controls.Add(this.checkBoxStep1Select);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(675, 430);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "FormMain";
             this.Text = "PrimeMon";
-            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.TopMost = true;
             this.groupBox1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcherMonitor)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -132,6 +235,12 @@
         private System.Windows.Forms.CheckBox checkBoxStep3Escape;
         private System.Windows.Forms.CheckBox checkBoxStep2Edit;
         private System.Windows.Forms.CheckBox checkBoxStep1Select;
+        private System.Windows.Forms.Timer timerExecute;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonReference;
     }
 }
 
--- PrimeMon/FormMain.cs
+++ PrimeMon/FormMain.cs
@@ -1,15 +1,10 @@
-﻿using System;
+﻿using Keyboard;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PrimeMon
@@ -17,88 +12,51 @@ namespace PrimeMon
     public partial class FormMain : Form
     {
         private string currentFile;
-        const string processName = "HPPrime";
-        private Random _random = new Random();
-        private static string _commandsFile;
+        const string processName = "HPPrime", referenceName = "PrimeHelp.exe";
         private string currentProgramName;
 
         public FormMain()
         {
             InitializeComponent();
+
+            Environment.CurrentDirectory = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+            if (File.Exists(referenceName))
+                buttonReference.Visible = true;
         }
 
         private void labelDragHere_DragEnter(object sender, DragEventArgs e)
         {
-          
-            e.Effect = DragDropEffects.All;
-            
+            foreach (var f in (String[])e.Data.GetData("FileName"))
+            {
+                if (Path.GetExtension(f).ToLower() == ".hpprgm")
+                {
+                    e.Effect = DragDropEffects.All;
+                }
+            }
         }
 
         private void labelDragHere_DragDrop(object sender, DragEventArgs e)
         {
             foreach (var f in (String[]) e.Data.GetData("FileName"))
             {
-                currentFile = f;
-                currentProgramName = Path.GetFileNameWithoutExtension(f);
-                labelDragHere.Text = "Monitoring '" + currentProgramName + "'";
-                fileSystemWatcherMonitor.Path = Path.GetDirectoryName(f);
-                fileSystemWatcherMonitor.EnableRaisingEvents = true;
-                break;
-            }
-        }
-
-        private void FormMain_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        Stopwatch lastRead = Stopwatch.StartNew();
-        object checking = new object();
-
-        private void fileSystemWatcherMonitor_Changed(object sender, FileSystemEventArgs e)
-        {
-            lock(checking)
-            { 
-                if (lastRead.ElapsedMilliseconds>5000)
+                if (Path.GetExtension(f).ToLower() == ".hpprgm")
                 {
-
-                    lastRead.Restart();
-
-                    if (string.Compare(e.FullPath, currentFile, true) == 0)
-                    {
-                        var p = "\"" + string.Join("\",{Wait},\"", currentProgramName.ToLower().ToCharArray()) + "\"";
-                        SendCommandToEmulator("{Show},{Focus},Escape,Escape,Escape,ControlKey_up,{Wait},NumPad1,ControlKey_down," + p);
-
-                        if (checkBoxStep2Edit.Checked)
-                        {
-                            SendCommandToEmulator("Enter");
-
-                            if (checkBoxStep3Escape.Checked)
-                            {
-                                SendCommandToEmulator("Escape");
-                            }
-                        }
-                    }
+                    currentFile = f;
+                    currentProgramName = Path.GetFileNameWithoutExtension(f);
+                    labelDragHere.Text = "Now monitoring '" + currentProgramName + "'";
+                    fileSystemWatcherMonitor.Path = Path.GetDirectoryName(f);
+                    fileSystemWatcherMonitor.EnableRaisingEvents = true;
+                    buttonEdit.Enabled = true;
+                    break;
                 }
             }
         }
 
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct KEYBDINPUT
+        private void fileSystemWatcherMonitor_Changed(object sender, FileSystemEventArgs e)
         {
-            public ushort wVk;
-            public ushort wScan;
-            public uint dwFlags;
-            public uint time;
-            public IntPtr dwExtraInfo;
+            if (string.Compare(e.FullPath, currentFile, true) == 0)
+                timerExecute.Start();
         }
-
-        [DllImport("user32.dll")]
-        public static extern IntPtr PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
-
-        [DllImport("user32.dll")]
-        public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
 
         [DllImport("user32.dll")]
         static extern bool ShowWindow(IntPtr hWnd, Int16 nCmdShow);
@@ -109,21 +67,37 @@ namespace PrimeMon
         const uint WM_KEYDOWN = 0x100, WM_KEYUP = 0x0101, WM_CHAR = 0x0102, WM_PASTE = 0x0302, WM_APPCOMMAND = 0x0319,
             APPCOMMAND_PASTE = 38, WM_SYSKEYDOWN = 0x0104, WM_SYSKEYUP = 0x0105, WM_COMMAND = 0x0111;
 
-        private void buttonRun_Click(object sender, EventArgs e)
+        private void checkBoxStep2Edit_CheckedChanged(object sender, EventArgs e)
         {
-            SendCommandToEmulator("ControlKey");
+            checkBoxStep3Escape.Enabled = checkBoxStep2Edit.Checked;
         }
 
-        private void buttonDebug_Click(object sender, EventArgs e)
+        private void buttonReference_Click(object sender, EventArgs e)
         {
-            SendCommandToEmulator("ControlKey_down");
+            try
+            {
+                Process.Start(referenceName);
+            }
+            catch { }
         }
 
-        private void buttonCheck_Click(object sender, EventArgs e)
+        private void buttonEdit_Click(object sender, EventArgs e)
         {
+            SendActionsToCalculator(true, false);
         }
 
-        public void SendCommandToEmulator(String command)
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void timerExecute_Tick(object sender, EventArgs e)
+        {
+            SendActionsToCalculator(checkBoxStep2Edit.Checked, checkBoxStep3Escape.Checked);
+            timerExecute.Stop();
+        }
+
+        public void SendActionsToCalculator(bool pressEnter, bool pressEscape)
         {
             var emulatorNotFound = true;
             foreach (var p in Process.GetProcesses())
@@ -133,154 +107,38 @@ namespace PrimeMon
                 emulatorNotFound = false;
                 var emulator = p.MainWindowHandle;
 
-                // Decode the keypresses
-                foreach (var k in command.Split(new[] { ',' }))
+                ShowWindow(emulator, 1);
+                SetForegroundWindow(emulator);
+
+                var k = new List<Key>();
+
+                for(var v=0;v<5;v++)
+                    k.Add(new Key(Messaging.VKeys.KEY_ESCAPE));
+
+                k.Add(new Key(Messaging.VKeys.KEY_CONTROL));
+                k.Add(new Key('1'));
+
+                foreach (var c in currentProgramName)
+                    k.Add(new Key(c));
+
+                if (pressEnter)
                 {
-                    var key = k.Trim();
-                    if (key.Length == 0) continue;
+                    k.Add(new Key(Messaging.VKeys.KEY_RETURN));
 
-                    switch (key)
-                    {
-                        /*case "{Text}":
-                        case "{Selection}":
-                        case "{CopyText}":
-                        case "{CopySelection}":
-                            var tmp = key.EndsWith("Text}") ? editor.Text : editor.Selection.Text;
-                            if (key.StartsWith("{Copy"))
-                                Clipboard.SetText(tmp);
-                            else
-                                foreach (var c in tmp)
-                                    SendKeyToWindow(emulator, IntPtr.Zero, c);
-                            break;*/
-
-                        case "{Nop}":
-                            Thread.Sleep(1);
-                            break;
-
-                        case "{Focus}":
-                            SetForegroundWindow(emulator);
-                            break;
-
-                        case "{Show}":
-                            ShowWindow(emulator, 1);
-                            break;
-
-                        case "{Paste}":
-                            PostMessage(emulator, WM_COMMAND, (IntPtr)32801, IntPtr.Zero);
-                            break;
-
-                        case "{Wait}":
-                                Thread.Sleep(10);
-                                Application.DoEvents();
-                            break;
-
-                        case "{Wait100}":
-                            for (int i = 0; i < 10; i++)
-                            {
-                                Thread.Sleep(10);
-                                Application.DoEvents();
-                            }
-                            break;
-
-                        default:
-                            if (key.EndsWith("}"))
-                            {
-                                if (key.StartsWith("{Alert:"))
-                                {
-                                    MessageBox.Show(key.Substring(7, key.Length - 8), "Alert");
-                                    continue;
-                                }
-
-                                if (key.StartsWith("{Question:"))
-                                {
-                                    if (
-                                        MessageBox.Show(key.Substring(10, key.Length - 11), "Question",
-                                            MessageBoxButtons.OKCancel) == DialogResult.Cancel)
-                                        return;
-                                    continue;
-                                }
-
-                                switch (key)
-                                {
-                                    /*case "{ProgramName}":
-                                        key = "\"" + CurrentProgramName + "\"";
-                                        break;*/
-
-                                    case "{RandomChar}":
-                                        key = "\"" + (char)_random.Next('a', 'z' + 1) + "\"";
-                                        break;
-
-                                    case "{RandomCHAR}":
-                                        key = "\"" + (char)_random.Next('A', 'Z' + 1) + "\"";
-                                        break;
-
-                                    case "{RandomNumber}":
-                                        key = "\"" + (char)_random.Next('0', '9' + 1) + "\"";
-                                        break;
-                                }
-                            }
-
-                            if (key.StartsWith("\"") && key.EndsWith("\""))
-                            {
-                                foreach (var c in key.Substring(1, key.Length - 2))
-                                    SendKeyToWindow(emulator, IntPtr.Zero, c);
-                            }
-
-                            bool keyUp = true, keyDown = true;
-                            if (key.EndsWith("_up"))
-                            {
-                                keyDown = false;
-                                key = key.Substring(0, key.Length - 3);
-                            }
-                            else if (key.EndsWith("_down"))
-                            {
-                                keyUp = false;
-                                key = key.Substring(0, key.Length - 5);
-                            }
-
-                            Keys pressedKey;
-                            if (Enum.TryParse(key, out pressedKey))
-                                SendKeyToWindow(emulator, (IntPtr)pressedKey, char.MinValue, keyUp, keyDown);
-                            break;
-                    }
+                    if(pressEscape)
+                        k.Add(new Key(Messaging.VKeys.KEY_ESCAPE));
                 }
+
+                foreach (Key key in k)
+                    key.Press(emulator,true);
             }
 
             if (emulatorNotFound)
             {
-                /*if (!String.IsNullOrEmpty(_parent.EmulatorExeFile))
-                {
-                    var p = Process.Start(_parent.EmulatorExeFile);
-                    p.WaitForInputIdle(1000);
-                    Thread.Sleep(100);
-                    SendCommandToEmulator(command);
-                }
-                else*/
-                    MessageBox.Show("The emulator is not running, and it seems it is not available in this system.",
-                        "Run Emulator", MessageBoxButtons.OK,
-                        MessageBoxIcon.Exclamation);
+                MessageBox.Show("The emulator is not running",
+                    "Run Emulator", MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
             }
         }
-
-        private static void SendKeyToWindow(IntPtr emulator, IntPtr key, char character, bool keyDown = true, bool keyUp = true)
-        {
-            if (key == IntPtr.Zero)
-            {
-                PostMessage(emulator, WM_CHAR, (IntPtr)character, IntPtr.Zero);
-                Thread.Sleep(1);
-            }
-            else
-            {
-                if (keyDown)
-                {
-                    PostMessage(emulator, WM_KEYDOWN, key, IntPtr.Zero);
-                    Thread.Sleep(1);
-                }
-
-                if (keyUp)
-                    PostMessage(emulator, WM_KEYUP, key, IntPtr.Zero);
-            }
-        }
-
     }
 }
